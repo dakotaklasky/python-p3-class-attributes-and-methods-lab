1ce670d1b282c383430e2b42c3858ab76d38a289
Dakota <dakota.klasky@gmail.com>
2024-08-29 15:33:54 -0400

    update cls reference

--- lib/song.py
+++ lib/song.py
@@ -16,30 +16,30 @@ class Song:
     
     @classmethod
     def add_to_song_count(cls):
-        Song.count += 1
+        cls.count += 1
     
     @classmethod
     def add_to_genres(cls, genre):
-        if genre not in Song.genres:
-            Song.genres.append(genre)
-        Song.add_to_genre_count(genre)
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        cls.add_to_genre_count(genre)
     
     @classmethod
     def add_to_artists(cls, artist):
-        if artist not in Song.artists:
-            Song.artists.append(artist)
-        Song.add_to_artist_count(artist)
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        cls.add_to_artist_count(artist)
     
     @classmethod
     def add_to_genre_count(cls,genre):
-        if genre in Song.genre_count:
-            Song.genre_count[genre] += 1
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
         else:
-            Song.genre_count[genre] = 1
+            cls.genre_count[genre] = 1
 
     @classmethod
     def add_to_artist_count(cls,artist):
-        if artist in Song.artist_count:
-            Song.artist_count[artist] += 1
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
         else:
-            Song.artist_count[artist] = 1
+            cls.artist_count[artist] = 1
